9a67c5ae7e3b901612d511faa0f45d23d6f226d6
Hernan Alvarez Caballero <hernandavidc@hotmail.com>
2021-11-04 21:15:53 -0500

    Intro Vuejs 2

--- intro-vuejs/components/vmodel.js
+++ intro-vuejs/components/vmodel.js
@@ -2,7 +2,7 @@ Vue.component('vmodel', {
     template: `
         <div>
             <h1> Bindeo de data con v-model </h1>
-            <input v-model="name" >
+            <input v-focus v-model="name" >
             <p> Hola {{ name }}, que bueno verte de nuevo </p>
         </div>
     `,
