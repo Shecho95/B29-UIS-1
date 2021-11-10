8fbd40bcf84d612c40c5bf85df80faa781c29adf
Hernan Alvarez Caballero <hernandavidc@hotmail.com>
2021-11-09 21:44:55 -0500

    Express seed project

--- express-base/index.js
+++ express-base/index.js
@@ -1,26 +1,5 @@
-const express = require('express');
-const app = express();
-const port = 5000;
+const Server = require('./models/server');
 
-app.use(express.json())
+const server = new Server();
 
-const { usersGet, 
-        usersPost,
-        usersGetByCC,
-        usersPut, 
-        usersDelete 
-    } = require('./controllers/ursers')
-
-app.get('/users', usersGet);
-
-app.post('/users', usersPost);
-
-app.get('/users/:cc', usersGetByCC);
-
-app.put('/users/:cc', usersPut);
-
-app.delete('/users/:cc', usersDelete);
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-});
+server.listen();
