17ced05dfce4f7e7f9698d5e707e5c4ab86fbded
Hernan Alvarez Caballero <hernandavidc@hotmail.com>
2021-11-24 00:36:07 -0500

    deploy

--- express-auth/models/server.js
+++ express-auth/models/server.js
@@ -7,8 +7,7 @@ class Server {
     
     constructor(){
         this.app = express();
-        // this.port = process.env.PORTSERVER | 3000;
-        this.port = 3000;
+        this.port = process.env.PORT || 5000;
         this.productsPath = '/api/products';
         this.usersPath = '/api/users';
 
